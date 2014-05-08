6df7c27ed51d2650698602a13211f9dca97274d8
Azeem Kahlon <aazeemm@gmail.com>
2014-05-08 18:02:15 +0500

    store

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
-  validates :title, :description, presence: true
-  validates :price, numericality: {greater_than_or_equal_to: 0.01}
-  validates :title, :uniqueness => true
+  validates :title, :price, :presence  => { :message => "This field is required."}
+  validates :title, :uniqueness => { :message => "Title must be unique." }
+  validates :title, length: {minimum: 4 ,:message => "Length should be of minimum 4 characters."}
+  validates :price, numericality: {greater_than_or_equal_to: 0.01 , :message => "Value should be numeric and greater than 0.01"}
 end
